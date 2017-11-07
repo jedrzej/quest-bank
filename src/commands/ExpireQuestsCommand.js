0e24ef9fecb8cdb1759a02fb9632553030bde884
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2017-11-06 22:14:17 -0400

    Reverted unintended changes to ExpireCommand

--- src/commands/ExpireQuestsCommand.js
+++ src/commands/ExpireQuestsCommand.js
@@ -8,24 +8,6 @@ export default class {
   }
 
   async execute() {
-
-    try {
-      await this.t("64f739a0-c33b-11e7-acd1-a982cbbd5675", "U123");
-      console.log('You just left a quest!');
-      return;
-
-      //bot.replyPrivate('You just joined a quest!');
-    } catch (e) {
-      console.log(e);
-      //bot.replyPrivate((typeof e === 'string') ? e : 'Whoops! There\'s been an error!');
-      console.log((typeof e === 'string') ? e : 'Whoops! There\'s been an error!');
-      return;
-    }
-
-
-
-
-
     const params = {
       FilterExpression: 'isComplete = :isComplete AND endDate <= :now',
       ExpressionAttributeValues: {':isComplete': false, ':now': moment().unix()}
@@ -37,30 +19,5 @@ export default class {
       quest.isComplete = true;
       this.questsService.update(quest.id, 'SET isComplete = :isComplete', {':isComplete': true});
     });
-  }
-
-  async t(questId, userId) {
-    const quest = await this.questsService.get(questId);
-    if (!quest) {
-      throw "Invalid questId";
-    }
-
-    quest.participants = quest.participants || [];
-
-    if (!quest.participants.includes(userId)) {
-      throw "You are not on this quest!";
-    }
-
-    const updatedParticipants = quest.participants.filter(participantId => participantId !== userId);
-
-    return this.questsService.update(
-      questId,
-      'SET participants = :updatedParticipants',
-      {
-        ':updatedParticipants': quest.updatedParticipants,
-        ':participants': quest.participants
-      },
-      'participants = :participants',
-    );
   }
 }

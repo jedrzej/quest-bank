7978a24f2517b926809bcf4fe26c7e440ef8c091
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2017-12-01 23:38:05 +0100

    Fied UpdateSettingsCommand

--- src/commands/UpdateSettingsCommand.js
+++ src/commands/UpdateSettingsCommand.js
@@ -1,15 +1,21 @@
 'use strict';
 
+import Logger from '../utils/Logger';
+
 export default class {
   constructor (slack, userSettingsService) {
     this.userSettingsService = userSettingsService;
+    this.logger = new Logger('UpdateSettingsCommand');
 
     slack.on('/quest-notify', async (msg, bot) => {
-      const matches = msg.trim().match(/(on|off)/);
+      this.logger.log('Processing message', msg.text);
+      const matches = msg.text.trim().match(/(on|off)/);
 
       if (!matches) {
         return bot.replyPrivate('Invalid option value');
       }
+
+      this.logger.log('Matches', matches);
 
       const enableNotifications = matches[1] === 'on';
 
@@ -17,6 +23,7 @@ export default class {
         await this.execute(msg.user_id, enableNotifications);
         return bot.replyPrivate(`Notifications have been ${enableNotifications ? 'enabled' : 'disabled'}`);
       } catch (e) {
+        this.logger.error(e);
         return bot.replyPrivate('Whoops! There\'s been an error!');
       }
     });

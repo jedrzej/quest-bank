b66dcf5ea7e7c9afd8ef8dcf363a459b170cef73
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2017-12-05 19:57:30 +0100

    Refactored UpdateSettingsCommand, added handling responses to interactive messages

--- src/commands/UpdateSettingsCommand.js
+++ src/commands/UpdateSettingsCommand.js
@@ -2,32 +2,65 @@
 
 import Logger from '../utils/Logger';
 
+const logger = new Logger('UpdateSettingsCommand');
+
 export default class {
   constructor(slack, userSettingsService) {
     this.userSettingsService = userSettingsService;
-    this.logger = new Logger('UpdateSettingsCommand');
 
-    slack.on('/quest-notify', async (msg, bot) => {
-      this.logger.log('Processing message', msg.text);
-      const matches = msg.text.trim().match(/(on|off)/);
-
-      if (!matches) {
-        return bot.replyPrivate('Invalid option value');
+    slack.on('/quest-notify', async (payload, client) => {
+      if (payload.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+        logger.log('Forbidden');
+        return;
       }
 
-      const enableNotifications = matches[1] === 'on';
+      this.client = client;
 
       try {
-        await this.execute(msg.user_id, enableNotifications);
-        return bot.replyPrivate(`Notifications have been ${enableNotifications ? 'enabled' : 'disabled'}`);
+        await this.handleCommand(payload)
       } catch (e) {
-        this.logger.error(e);
-        return bot.replyPrivate('Whoops! There\'s been an error!');
+        logger.log('ERROR', e.message);
+      }
+    });
+
+    slack.on('*', async (payload, client) => {
+      if (payload.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+        logger.log('Forbidden');
+        return;
+      }
+
+      this.client = client;
+
+      try {
+        await this.handleResponse(payload)
+      } catch (e) {
+        logger.log('ERROR', e.message);
       }
     });
   }
 
-  execute(userId, enableNotifications) {
+  async handleCommand(payload) {
+    const matches = payload.text.trim().match(/(on|off)/);
+    if (!matches) {
+      return this.client.replyPrivate('Invalid option value');
+    }
+
+    const enableNotifications = matches[1] === 'on';
+    await this.updateEnableNotificationsSetting(payload.user_id, enableNotifications);
+    return bot.replyPrivate(`Notifications have been ${enableNotifications ? 'enabled' : 'disabled'}`);
+  }
+
+  async handleResponse(payload) {
+    if (payload.type === 'interactive_message' && payload.actions.length && payload.actions[0].name === 'disable_notifications') {
+      await this.updateEnableNotificationsSetting(payload.user.id, false);
+      return bot.replyPrivate('Notifications have been disabled');
+    } else if (payload.type === 'interactive_message' && payload.actions.length && payload.actions[0].name === 'enable_notifications') {
+      await this.updateEnableNotificationsSetting(payload.user.id, true);
+      return bot.replyPrivate('Notifications have been false');
+    }
+  }
+
+  updateEnableNotificationsSetting(userId, enableNotifications) {
     return this.userSettingsService.put({
       id: userId,
       enableNotifications
